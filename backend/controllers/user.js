c09d369ea442b9cae8c7b94fe3ee4bf207665c8e
matheusmaldonadosilva <matheussilvacontat@gmail.com>
2023-04-29 13:53:06 -0300

    update project facebook clone

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,38 @@
-exports.home = (req, res) => {
-    res.send('Welcome');
-    res.status(403).json({
-        message: 'Invalid',
-        error: 'Invalid'
-    })
+const User = require('../models/User');
+
+exports.register = async (req, res) => {
+    try {
+    const {
+        first_name,
+        last_name,
+        email,
+        password,
+        username,
+        bYear,
+        bMonth,
+        bDay,
+        gender
+    } = req.body;
+
+    const user = await new User({
+        first_name,
+        last_name,
+        email,
+        password,
+        username,
+        bYear,
+        bMonth,
+        bDay,
+        gender
+    }).save();
+    res.json({
+        success: true,
+        user: user
+    });
+    } catch(err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
 };    
