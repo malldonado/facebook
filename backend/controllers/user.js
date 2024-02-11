6e0d24ba4d1a5871511166405393382c0f9eb2f5
malldonado <matheusmaldonadosilva@outlook.com>
2024-02-11 12:23:01 -0300

    update project

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,6 @@
+const { validateEmail, validateLenght } = require('../helpers/validation');
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 exports.register = async (req, res) => {
     try {
@@ -14,6 +16,39 @@ exports.register = async (req, res) => {
         gender
     } = req.body;
 
+    if(!validateEmail(email)) {
+        return res.status(400).json({
+            message: "Invalid email address",
+        });
+    }
+
+    const check = await User.findOne({ email });
+    if(check) {
+        return res.status(400).json({
+            message: "This emails address already exists, try with a different email address",
+        });
+    }
+
+    if(!validateLenght(first_name, 3, 30)) {
+        return res.status(400).json({
+            message: "First name must between 3 and 30 characters"
+        });
+    }
+
+    if(!validateLenght(last_name, 3, 30)) {
+        return res.status(400).json({
+            message: "Last name must between 3 and 30 characters"
+        });
+    }
+
+    if(!validateLenght(password, 6, 30)) {
+        return res.status(400).json({
+            message: "Password must be atleast 6 characters"
+        });
+    }
+    const cryptedPassword = await bcrypt.hash(password, 12);
+    console.log(cryptedPassword);
+    return;
     const user = await new User({
         first_name,
         last_name,
